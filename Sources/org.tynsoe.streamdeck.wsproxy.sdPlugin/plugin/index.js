20ab259ee6afa64f27e06261da59b4dc08f6c34c
Yann Bizeul <yann@tynsoe.org>
2020-11-24 11:31:00 +0100

    Comment majority of the code

--- Sources/org.tynsoe.streamdeck.wsproxy.sdPlugin/plugin/index.js
+++ Sources/org.tynsoe.streamdeck.wsproxy.sdPlugin/plugin/index.js
@@ -1,25 +1,77 @@
-var websocket = null;  // Websocket to Streamdeck
-var connections = {};  // Dictionary maintaining connections for each key
+var websocket = null;  // Websocket to Stream Deck
 
+/*
+// Dictionary maintaining connections for each key
+The structure is the following :
+
+{
+	"ws://mysocket1/path" : {
+		"websocket": websocket1,
+		"positions": ["position1","position2"]
+	},
+	"ws://mysocket2/path" : {
+		"websocket": websocket2,
+		"positions": ["position3"]
+	},
+}
+
+Each socket maintains an array of positions it is configured for so that
+when there is no positions left we can disconnect, but as long as there
+is at least one position listed, we leave the connection open. The format
+of the position string is "<column>-<row>" but the actual value is never used,
+it is there just to maintain count of how many keys are using the socket.
+*/
+var connections = {};
+
+// Adds the given position in the "positions" array for the server
 function addPositionForServer(server,position) {
 	index=connections[server].positions.indexOf(position)
 	if (index == -1) {
 		connections[server].positions.push(position)
 	}
 }
+
+// Removes the given position from the "positions" array for the server
+// After calling this, the caller usually checks if there is any positions
+// left and if not, disconnects the socket and removes the entry from then
+// connections object.
 function removePositionForServer(server,position) {
 	index=connections[server].positions.indexOf(position)
 	if (index != -1) {
 		connections[server].positions.splice(index,1)
 	}
 }
+
+// Converts positions dict as given by Stream Deck SDK to a simple string used
+// in the connections object
 function positionFromCoordinates(c) {
 	return c.column + "-" + c.row
 }
+
+/*
+Connects to a given server.
+
+This is usually triggered by an willAppear message coming from Stream Deck.
+
+If there is an existing websocket, it will be used instead of creating a new
+one, and the position will be added to its "positions" array.
+
+message is used to send an initial message after connection is established.
+Used with the initial `willAppear`message coming from the Stream Deck.
+
+backend_only is used to handle server side disconnect, when it happens, we
+call this method with backend_only to true, which creates a new socket without
+changing any of the positions already registered.
+*/
 function connect(remoteServer,position,message,backend_only=false) {
-	// Close connection if it already exists
+
+	// The connection to this server already exists
 	if (connections.hasOwnProperty(remoteServer) && (backend_only == false)) {
 		addPositionForServer(remoteServer,position)
+
+		// When starting up, all the keys are rapidly sent but the connection is
+		// not available when keys after the very first one are added, so we
+		// simply wait a bit and retry the send message
 		if (connections[remoteServer].websocket.readyState == 0) {
 			setTimeout(() => {
 				connections[remoteServer].websocket.send(JSON.stringify(message))
@@ -31,16 +83,24 @@ function connect(remoteServer,position,message,backend_only=false) {
 		return
 	}
 
-	// Create a new connection to the remote websocket
+	// No socket exists (or it does and backend_only is true), so we create a
+	// new one
 	c = new WebSocket(remoteServer)
 	if (backend_only) {
+		// We are reconnecting, so keep existing object and just update
+		// websocket
 		connections[remoteServer].websocket=c
 	}
 	else {
+		// This is a brand new connection, add the connection object to
+		// connections
 		connections[remoteServer] = {positions: [position],websocket: c}
 	}
 
-	// send willAppear message when connection is ready
+	// Connection handlers.
+
+	// When the connection is established, we might have a message to send.
+	// This is usually the "willAppear" message that triggered the connection
 	c.onopen = function(evt) {
 		console.log("Remote socket opened")
 		if (message) {
@@ -48,7 +108,7 @@ function connect(remoteServer,position,message,backend_only=false) {
 		}
 	}
 
-	// Forward to StreamDeck incoming messages
+	// Forward any incomming message to Stream Deck
 	c.onmessage = function(evt) {
 		j=JSON.parse(evt.data)
 		console.log("Forwarding message")
@@ -57,6 +117,8 @@ function connect(remoteServer,position,message,backend_only=false) {
 			websocket.send(JSON.stringify(j))
 		}
 	}
+
+	// Looks like server disconnected, reconnect with backend_only=true
 	c.onclose = function() {
 		if (connections.hasOwnProperty(key)) {
 			connect(remoteServer,null,null,true)
@@ -64,11 +126,21 @@ function connect(remoteServer,position,message,backend_only=false) {
 	}
 }
 
-function disconnect(remoteServer,position,message) {
+/*
+Disconnects a websocket
 
+This method is usually called from a `willDisappear` message.
+
+It does not actually disconnects the server unless there is no
+positions listed for it.
+
+We also send the message if the socket is available, usually the
+`willDisappear` message.
+*/
+function disconnect(remoteServer,position,message) {
 	if (connections.hasOwnProperty(remoteServer)) {
 		c=connections[remoteServer].websocket;
-		if (c.readyState == 1) {
+		if (c.readyState == 1 && message) {
 			c.send(JSON.stringify(message))
 		}
 		removePositionForServer(remoteServer,position)
@@ -79,6 +151,10 @@ function disconnect(remoteServer,position,message) {
 		}
 	}
 }
+
+/*
+Initial communication with the Stream Deck software.
+*/
 function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo) {
 	pluginUUID = inPluginUUID
 
@@ -115,6 +191,10 @@ function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, in
 
 			connect(settings.remoteServer,positionFromCoordinates(coordinates))
 		}
+		/*
+		willAppear usually triggers a connection to the remote socket, or
+		reuse an existing one.
+		*/
 		else if(event == "willAppear")
 		{
 			var jsonPayload = jsonObj['payload'];
@@ -124,6 +204,10 @@ function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, in
 				connect(settings.remoteServer,positionFromCoordinates(coordinates),jsonObj)
 			}
 		}
+		/*
+		willDisappear usually triggers a disconnection from the remote socket,
+		or unregister this key fromt the connection.
+		*/
 		else if(event == "willDisappear")
 		{
 			var jsonPayload = jsonObj['payload'];
@@ -132,7 +216,11 @@ function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, in
 
 			disconnect(settings.remoteServer,positionFromCoordinates(coordinates),jsonObj)
 		}
-		// If there is a key associated with the message, forward to remote websocket
+		/*
+		Every other message is simply forwarded to node-red, the condition is
+		that the message must have a settings.remoteServer setting, which is the
+		case for any key-related events.
+		*/
 		else if (jsonObj.hasOwnProperty("payload")) {
 			if (jsonObj['payload'].hasOwnProperty("settings")) {
 				key = jsonObj['payload']['settings']['remoteServer']
